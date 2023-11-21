4dfb911b1b9c59adebe5c857a68d318f9b65865f
1261082413 <dhy1261082413@gmail.com>
2023-11-21 17:20:03 -0500

    update healthUI and revised the AI of the enemies to make their behavior more dynamic and realistic

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,49 +7,120 @@ public class EnemyController : MonoBehaviour
     public Rigidbody2D theRB;
     public float speed;
 
-    public float rangeToPlayer;
+    public float rangeToChasePlayer;
+    public float rangeToAttackPlayer;
 
     public Animator anim;
 
-    public Vector3 move;
+    public Vector3 moveDirection;
 
     public int health = 150;
+
+    public GameObject[] deathSplatter;
+    public GameObject hitEffect;
+
+    public bool shouldShoot;
+    public GameObject bullet;
+
+    public Transform firePoint;
+    public float fireRate;
+
+    private float fireCounter;
+    private float patrolCounter;
+    private Vector3 patrolDestination;
+
+    public SpriteRenderer theBody;
+
+    private enum EnemyState { Idle, Patrolling, Chasing, Attacking }
+    private EnemyState currentState = EnemyState.Idle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize patrol destination or other variables if needed
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position,PlayerController.instance.transform.position) < rangeToPlayer)
+        switch (currentState)
         {
-            move = PlayerController.instance.transform.position - transform.position;
-        }else{
-            move = Vector3.zero;
+            case EnemyState.Idle:
+                // Wait or perform simple behavior
+                break;
+            case EnemyState.Patrolling:
+                Patrol();
+                break;
+            case EnemyState.Chasing:
+                ChasePlayer();
+                break;
+            case EnemyState.Attacking:
+                AttackPlayer();
+                break;
         }
-        move.Normalize();
-        move.z = 0;
-        transform.position += move *speed;
-        //theRB.velocity = move * speed;
 
-        if(move != Vector3.zero)
+        UpdateState();
+        UpdateAnimation();
+    }
+
+    void Patrol()
+    {
+        // Implement patrol logic here
+        // Example: Move to a new random position after certain time
+    }
+
+    void ChasePlayer()
+    {
+        moveDirection = PlayerController.instance.transform.position - transform.position;
+        moveDirection.Normalize();
+        transform.position += moveDirection * speed * Time.deltaTime;
+    }
+
+    void AttackPlayer()
+    {
+        // Implement different types of attacks here
+        // Example: Melee, Ranged, etc.
+    }
+
+    void UpdateState()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, PlayerController.instance.transform.position);
+        if (distanceToPlayer < rangeToAttackPlayer)
+        {
+            currentState = EnemyState.Attacking;
+        }
+        else if (distanceToPlayer < rangeToChasePlayer)
+        {
+            currentState = EnemyState.Chasing;
+        }
+        else
+        {
+            currentState = EnemyState.Patrolling;
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        if (moveDirection != Vector3.zero)
         {
             anim.SetBool("isMoving", true);
-        }else
+        }
+        else
         {
             anim.SetBool("isMoving", false);
-
         }
-
     }
+
     public void DamageEnemy(int damage)
     {
         health -= damage;
+        Instantiate(hitEffect, transform.position, transform.rotation);
 
-        if(health <= 0)
+        if (health <= 0)
         {
+            int selectedSplatter = Random.Range(0, deathSplatter.Length);
+            int rotation = Random.Range(0, 4);
+            Instantiate(deathSplatter[selectedSplatter], transform.position, Quaternion.Euler(0f, 0f, rotation * 90f));
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth;
+
+    public int maxHealth;
+
+    public string example;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UIController.instance.healthSlider.maxValue = maxHealth;
+        UIController.instance.healthSlider.value = currentHealth;
+        UIController.instance.healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DamagePlayer()
+    {
+        currentHealth-- ;
+        if(currentHealth <=0)
+        {
+            PlayerHealthController.instance.gameObject.SetActive(false);
+
+        }
+
+        UIController.instance.healthSlider.value = currentHealth;
+        UIController.instance.healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
+    }
+}
