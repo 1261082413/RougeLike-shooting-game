6317fdf3b03934353c6488548876ca139b2e1c3f
1261082413 <dhy1261082413@gmail.com>
2024-03-28 23:07:59 -0400

    add activator of dialog sysytem

--- Assets/Scripts/Dialog Manager.cs
+++ Assets/Scripts/Dialog Manager.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class DialogManager : MonoBehaviour
 {
-    public Text dialogText;
-    public Text nameText;
+    public TextMeshProUGUI dialogText;
+    public TextMeshProUGUI nameText;
     public GameObject dialogBox;
     public GameObject nameBox;
 
     public string[] dialogLines;
     public int currentLine;
+    public static DialogManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        dialogText.text = dialogLines[currentLine];
+        instance =this;
+        //dialogText.text = dialogLines[currentLine];
     }
 
     // Update is called once per frame
@@ -25,9 +28,25 @@ public class DialogManager : MonoBehaviour
            if(Input.GetButtonUp("Fire1")) 
            {
             currentLine ++;
-            dialogText.text = dialogLines[currentLine];
+            if(currentLine >= dialogLines.Length)
+            {
+                dialogBox.SetActive(false);
+            }else
+            {
+                dialogText.text = dialogLines[currentLine];
+            }
+
+            
 
            }
         }
     }
+    public void ShowDialog(string[] newLines)
+        {
+            dialogLines =newLines;
+            currentLine = 0;
+            dialogtext.text= dialogLines[0];
+            dialogBox.SeActive(true);
+        }
+    
 }
